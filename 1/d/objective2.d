bb90f53c3b55a31782bd699713f95c822f706088
Matt Bailey <bailey.matthewr@gmail.com>
2021-03-17 10:55:36 -0600

    Add examples in d

---
+++ 1/d/objective2.d
@@ -0,0 +1,9 @@
+import std;
+
+void main () {
+    int i = 1;
+    while(i < 6){
+        writeln(i);
+        i++;
+    }
+}
---
+++ 1/d/objective3.d
@@ -0,0 +1,6 @@
+import std;
+
+void main(){
+    for(int i = 1 ; i < 6; ++i)
+        writeln(i);
+}
