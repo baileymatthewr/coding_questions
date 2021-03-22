85e23544d01bedab6e174abfe12b7b932557fd69
Matt Bailey <bailey.matthewr@gmail.com>
2021-03-21 18:10:08 -0600

    Add new examples in c++

---
+++ 1/c++/objective2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[]){
+    int i = 1;
+    while(i < 6){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
---
+++ 1/c++/objective3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[]){
+    for(int i = 1; i < 6; ++i)
+        cout << i << endl;
+    return 0;
+}
