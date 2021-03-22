233b61f00c026291265a38c4987a481e547c6f9a
Matt Bailey <bailey.matthewr@gmail.com>
2021-03-21 18:52:42 -0600

    Add examples in c#

---
+++ 1/c#/objective2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace objective1 {
+    class MainClass {
+        static void Main(){
+            int i = 1;
+            while(i < 6){
+                Console.WriteLine("{0:D}", i);
+                i++;
+            }
+        }
+    }
+}
---
+++ 1/c#/objective3.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace objective1 {
+    class MainClass {
+        static void Main(){
+            for(int i = 1; i < 6; ++i)
+                Console.WriteLine("{0:D}", i);
+        }
+    }
+}
